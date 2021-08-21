f735fac19ba6c3754367c0212a2486c5c7c83c48
JosepAnSabaté <josep.andreu@e-campus.uab.cat>
2021-08-21 12:27:15 +0200

    layers

---
+++ ol_node_app_layers/main.js
@@ -0,0 +1,31 @@
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import Stamen from 'ol/source/Stamen';
+import Group from 'ol/layer/Group';
+
+const map = new Map({
+  target: 'map',
+  layers:  [new Group({
+    'title': 'Base maps',
+    layers: [
+     // new TileLayer({
+      new TileLayer({
+        source: new Stamen({
+          layer: 'watercolor',
+        }),
+      }),
+     new TileLayer({
+        source: new Stamen({
+          layer: 'watercolor',
+        }),
+      }),
+      //   }),
+     ]
+    })],
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
+
